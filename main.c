e6c28225df9585accf064758ba67fdf7aaf34e8c
GuiCastro7 <d2024010605@unifei.edu.br>
2024-05-07 14:18:04 +0000

    Primeira versão

---
+++ main.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+#define BUFFER_SIZE 1024
+
+void handle_request(int client_socket) {
+    char buffer[BUFFER_SIZE];
+    char response[] = "HTTP/1.1 200 OK\r\n"
+                      "Content-Type: text/html\r\n"
+                      "\r\n"
+                      "<html><body><h1>Fuck you!</h1></body></html>";
+
+    recv(client_socket, buffer, BUFFER_SIZE, 0);
+    printf("Received request: %s\n", buffer);
+
+    send(client_socket, response, strlen(response), 0);
+    close(client_socket);
+}
+
+int main() {
+    int server_socket, client_socket;
+    struct sockaddr_in server_address, client_address;
+    socklen_t client_length = sizeof(client_address);
+
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_socket < 0) {
+        perror("socket creation failed");
+        exit(1);
+    }
+
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(PORT);
+    inet_pton(AF_INET, "127.0.0.1", &server_address.sin_addr);
+
+    if (bind(server_socket, (struct sockaddr *)&server_address, sizeof(server_address)) < 0) {
+        perror("bind failed");
+        exit(1);
+    }
+
+    if (listen(server_socket, 3) < 0) {
+        perror("listen failed");
+        exit(1);
+    }
+
+    printf("Server started on port %d...\n", PORT);
+
+    while (1) {
+        client_socket = accept(server_socket, (struct sockaddr *)&client_address, &client_length);
+        if (client_socket < 0) {
+            perror("accept failed");
+            continue;
+        }
+
+        printf("Connection accepted...\n");
+        handle_request(client_socket);
+    }
+
+    return 0;
+}
